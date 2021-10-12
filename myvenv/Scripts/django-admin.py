dcdb54d2a06d894e950c7cda3058b896b62d0d38
tetsuya <iimuro@fujicars.jp>
2021-10-12 16:36:34 +0900

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Tetsuya Iimuro\desktop\django_desktop\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python_intro.py
@@ -0,0 +1,18 @@
+print('Hello, Django girls!')
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than2')
+
+
+def hi(name):
+    print('hi ' + name + '!')
+
+girls = ['Rachal', 'Monica', 'Probe', 'Ola', 'you']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+
+for i in range(1,6):
+    print(i)
